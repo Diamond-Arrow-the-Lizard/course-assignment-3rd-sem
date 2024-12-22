d330bce1456926c5578494902b5b28d2a717f2f7
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-22 21:23:10 +0700

    Made fixes for every other Services for Modules

--- Services/FlightCrewService.cs
+++ Services/FlightCrewService.cs
@@ -4,44 +4,48 @@ namespace AirlinesSystem.Services;
 public class FlightCrewService : IFlightCrewService
 {
     private readonly IJsonHelper _jsonHelper;
+    private readonly string _dataPath;
 
     public FlightCrewService(IJsonHelper jsonHelper)
     {
-        _jsonHelper = jsonHelper;
+        _jsonHelper = jsonHelper ?? throw new ArgumentNullException(nameof(jsonHelper));
+        _dataPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Data", "FlightCrews.json");
+        //Console.WriteLine($"Path to FlightCrew data file: {_dataPath}");
     }
 
     public async Task<IEnumerable<IFlightCrew>> GetAllFlightCrewsAsync()
     {
-        var json = await File.ReadAllTextAsync("FlightCrews.json");
-        return await _jsonHelper.DeserializeAsync<List<IFlightCrew>>(json);
+        var json = await File.ReadAllTextAsync(_dataPath);
+        return await _jsonHelper.DeserializeAsync<List<FlightCrew>>(json);
     }
 
     public async Task<IFlightCrew> GetFlightCrewByIdAsync(int id)
     {
-        var json = await File.ReadAllTextAsync("FlightCrews.json");
-        var FlightCrews = await _jsonHelper.DeserializeAsync<List<IFlightCrew>>(json);
+        var json = await File.ReadAllTextAsync(_dataPath);
+        var FlightCrews = await _jsonHelper.DeserializeAsync<List<FlightCrew>>(json);
         return FlightCrews.FirstOrDefault(a => a.FlightId == id) ?? throw new Exception("Crew member wasn't found");
     }
 
     public async Task AddFlightCrewAsync(IFlightCrew FlightCrew)
     {
-        var json = await File.ReadAllTextAsync("FlightCrews.json");
-        var FlightCrews = await _jsonHelper.DeserializeAsync<List<IFlightCrew>>(json);
-        FlightCrews.Add(FlightCrew);
+        var json = await File.ReadAllTextAsync(_dataPath);
+        var FlightCrews = await _jsonHelper.DeserializeAsync<List<FlightCrew>>(json);
+        if (FlightCrew is FlightCrew a)
+            FlightCrews.Add(a);
         var updatedJson = await _jsonHelper.SerializeAsync(FlightCrews);
-        await File.WriteAllTextAsync("FlightCrews.json", updatedJson);
+        await File.WriteAllTextAsync(_dataPath, updatedJson);
     }
 
     public async Task RemoveFlightCrewAsync(int id)
     {
-        var json = await File.ReadAllTextAsync("FlightCrews.json");
-        var FlightCrews = await _jsonHelper.DeserializeAsync<List<IFlightCrew>>(json);
+        var json = await File.ReadAllTextAsync(_dataPath);
+        var FlightCrews = await _jsonHelper.DeserializeAsync<List<FlightCrew>>(json);
         var FlightCrew = FlightCrews.FirstOrDefault(a => a.FlightId == id);
         if (FlightCrew != null)
         {
             FlightCrews.Remove(FlightCrew);
             var updatedJson = await _jsonHelper.SerializeAsync(FlightCrews);
-            await File.WriteAllTextAsync("FlightCrews.json", updatedJson);
+            await File.WriteAllTextAsync(_dataPath, updatedJson);
         }
     }
 }

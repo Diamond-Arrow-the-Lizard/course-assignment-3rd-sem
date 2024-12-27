172ff96413a3e06bee39345e2f84e1f0e2d4179d
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-27 13:37:01 +0700

    Updated TicketView to use DI

--- AvaloniaApp/Views/TicketView.axaml.cs
+++ AvaloniaApp/Views/TicketView.axaml.cs
@@ -3,14 +3,18 @@ using AirlinesSystem.Services;
 using AirlinesSystem.Utilities;
 using Avalonia.Controls;
 using AvaloniaApp.ViewModels;
+using System;
 
 namespace AvaloniaApp.Views;
 
-public partial class TicketView : UserControl 
+public partial class TicketView : UserControl
 {
     public TicketView()
     {
         InitializeComponent();
-        this.DataContext = new TicketViewModel(new TicketService(new JsonHelper()));
+
+        if (Avalonia.Application.Current != null)
+            this.DataContext = ((App)Avalonia.Application.Current).GetService<TicketViewModel>();
+        else throw new InvalidOperationException("ServiceProvider is not initialized.");
     }
 }

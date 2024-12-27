2d3c504fd3ac30827b218df42b67d77a15221ba3
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-27 11:50:52 +0700

    Made TicketView visible on a main window

---
+++ AvaloniaApp/Views/TicketView.axaml.cs
@@ -0,0 +1,12 @@
+
+using Avalonia.Controls;
+
+namespace AvaloniaApp.Views;
+
+public partial class TicketView : UserControl 
+{
+    public TicketView()
+    {
+        InitializeComponent();
+    }
+}

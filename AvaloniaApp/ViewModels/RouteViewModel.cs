9c1d4d56382698239a93cb28d0b9b73f634a283c
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-27 14:33:20 +0700

    Added files and usings for RouteView

---
+++ AvaloniaApp/ViewModels/RouteViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using AirlinesSystem.Interfaces;
+using AirlinesSystem.Modules;
+using AirlinesSystem.Services;
+using Avalonia.Controls;
+
+namespace AvaloniaApp.ViewModels;
---
+++ AvaloniaApp/Views/RouteView.axaml.cs
@@ -0,0 +1,8 @@
+using AirlinesSystem.Services;
+using AirlinesSystem.Utilities;
+using AirlinesSystem.Modules;
+using Avalonia.Controls;
+using AvaloniaApp.ViewModels;
+using System;
+
+namespace AvaloniaApp.Views;

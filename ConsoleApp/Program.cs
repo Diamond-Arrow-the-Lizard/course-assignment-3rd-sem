99561861c889417d0dd2d3a085f218b7d0ede819
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-29 21:53:22 +0700

    Does nothing for now

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,6 +13,8 @@ public class Program
 {
     static async Task Main(string[] args)
     {
+        await Task.Delay(0);
+
         // Настройка Dependency Injection
         var services = new ServiceCollection();
 
@@ -23,7 +25,6 @@ public class Program
         services.AddSingleton<IAircraftService, AircraftService>();
         services.AddSingleton<IRouteService, RouteService>();
         services.AddSingleton<ITicketService, TicketService>();
-        services.AddSingleton<ISearchService, SearchService>();
 
         var serviceProvider = services.BuildServiceProvider();
 
@@ -37,29 +38,7 @@ public class Program
 
         var routeService = serviceProvider.GetService<IRouteService>();
         var ticketService = serviceProvider.GetService<ITicketService>();
-        var searchService = serviceProvider.GetService<ISearchService>();
 
-        var newRoute = new Route
-        (
-            "A1080",
-            "Moscow",
-            DateTime.Today,
-            "Magadan",
-            new DateTime(2024, 12, 31),
-            "12347",
-            [],
-            [DayOfWeek.Sunday, DayOfWeek.Monday, DayOfWeek.Tuesday]
-        );
-
-        if (routeService != null)
-        {
-            await routeService.AddRouteAsync(newRoute);
-            var routeCopy = await routeService.GetRouteByIdAsync("A1080");
-            Console.WriteLine(routeCopy.RouteId);
-        }
-        if (aircraftService != null) {
-            Console.WriteLine((await aircraftService.GetAircraftByIdAsync("12347")).Type);
-        }
 
     }
 

1b39262e3c62a3eee795cad747bee0011451f2c1
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-29 21:54:06 +0700

    Deleted for being useless

--- Interfaces/ISearchService.cs
+++
@@ -1,9 +0,0 @@
-
-namespace AirlinesSystem.Interfaces;
-
-public interface ISearchService
-{
-    Task<IEnumerable<IAircraft>> SearchAircraftsAsync(string query);
-    Task<IEnumerable<IRoute>> SearchRoutesAsync(string query);
-    Task<IEnumerable<ITicket>> SearchTicketsAsync(string query);
-}

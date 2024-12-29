3a74d64bdf60d6d3727418c3d7c52e0b953c8348
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-29 20:48:08 +0700

    Implemented TicketListView

--- AvaloniaApp/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApp/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,78 @@
-﻿using AirlinesSystem.Interfaces;
+﻿using AvaloniaApp.Views;
+using AirlinesSystem.Interfaces;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
 
-namespace AvaloniaApp.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
+namespace AvaloniaApp.ViewModels
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
-   
-    private readonly ITicketService _ticketService;
-
-    public MainWindowViewModel(ITicketService ticketService)
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
-        _ticketService = ticketService;
-        SelectedTicketViewModel = new TicketViewModel(_ticketService);
+        private readonly ITicketService _ticketService;
+
+        private object _currentView = new RouteView();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand ShowRouteViewCommand { get; }
+        public ICommand ShowTicketListViewCommand { get; }
+
+        public MainWindowViewModel(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+            SelectedTicketViewModel = new TicketViewModel(_ticketService);
+
+            ShowRouteViewCommand = new RelayCommand(ShowRouteView);
+            ShowTicketListViewCommand = new RelayCommand(ShowTicketListView);
+        }
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                if (_currentView != value)
+                {
+                    _currentView = value;
+                    OnPropertyChanged(nameof(CurrentView));
+                }
+            }
+        }
+
+        public TicketViewModel SelectedTicketViewModel { get; }
+
+        private void ShowRouteView() => CurrentView = new RouteView();
+
+        private void ShowTicketListView() => CurrentView = new TicketListView();
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-    public TicketViewModel SelectedTicketViewModel { get; }
-    
+
+    // Простой класс RelayCommand для реализации ICommand
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        // Метод, определяющий, можно ли выполнить команду
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+        // Метод для уведомления об изменении состояния CanExecute
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
 }

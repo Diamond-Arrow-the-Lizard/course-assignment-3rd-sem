983027f2cce4ec09291e6de3b36867567ece71c1
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-29 21:52:59 +0700

    Refactored code to make data more connected

--- Modules/CrewMember.cs
+++ Modules/CrewMember.cs
@@ -7,15 +7,11 @@ public class CrewMember : ICrewMember
 {
     public string CrewMemberId { get; } = "Undefined";
     public string Name { get; set; } = "Undefined";
-    public string Position { get; set; } = "Undefined";
-    public DateTime DateOfBirth { get; set; }
 
     [JsonConstructor]
-    public CrewMember(string CrewMemberId, string Name, string Position, DateTime DateOfBirth)
+    public CrewMember(string CrewMemberId, string Name)
     {
         this.CrewMemberId = CrewMemberId;
         this.Name = Name;
-        this.Position = Position;
-        this.DateOfBirth = DateOfBirth;
     }
 }

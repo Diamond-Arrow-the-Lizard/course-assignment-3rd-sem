f58b242c34e5d4646437094ef81f3b7dbe9ffba0
Diamond Arrow on Windows 10 <diamond1arrow@gmail.com>
2024-12-29 21:54:06 +0700

    Deleted for being useless

--- Interfaces/IScheduleService.cs
+++
@@ -1,9 +0,0 @@
-
-namespace AirlinesSystem.Interfaces;
-
-public interface IScheduleService
-{
-    List<IRoute> GetRoutesByDay(string dayOfWeek);
-    List<IFlightCrew> GetFlightCrewsForDate(DateTime date);
-    List<ITicket> GetTicketsForFlight(string flightNumber, DateTime flightDate);
-}
